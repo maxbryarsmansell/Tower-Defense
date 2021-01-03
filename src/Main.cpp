4fd13abb7ae27307476890774c0bd10b946e19c9
Max Bryars-Mansell <maxbm46@gmail.com>
2021-01-03 20:45:15 +0000

    Test commit.

--- src/Main.cpp
+++ src/Main.cpp
@@ -17,7 +17,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Window", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
